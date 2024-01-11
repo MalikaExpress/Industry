d524883467b5e18c571a38709357720f6dfe3473
Malika_Day_One <anaka3637@Gmail.com>
2024-01-11 09:03:36 +0800

    first commit

---
+++ BMI.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BMI{
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Masukkan Nama");
+        String nama = input.nextLine();
+        System.out.println("Masukkan Tinggi");
+        double tinggi = input.nextDouble();
+        System.out.println("Masukkan Berat");
+        double berat = input.nextDouble();
+
+        String kategori = null;
+        double BMI = berat/(tinggi*tinggi);
+
+        if(BMI <17){
+            kategori="kurus Berat";
+        }else if (BMI >=17 && BMI <=18.4){
+            kategori="Kurus Ringan";
+        }else if (BMI >=18.5 && BMI <=25.0){
+            kategori="Normal";
+        }else if (BMI >=25.1 && BMI <=27.0){
+            kategori="Gemuk Ringan";
+        }else if (BMI >=27){
+            kategori="Gemuk Berat";
+        }
+
+        System.out.println("Masukkan BMI");
+        System.out.println(nama);
+        System.out.println(BMI);
+        System.out.println(kategori);
+
+
+
+        
+    }
+
+
+
+
+}
